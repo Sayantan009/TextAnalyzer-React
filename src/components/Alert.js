2b3a5e350289f31dab83d6234e555384f2a86430
Sayantan <131001613+Sayantan009@users.noreply.github.com>
2023-10-05 10:21:33 +0530

    React App

---
+++ src/components/Alert.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Alert(props) {
+  const Cap = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1)
+  }
+  return (
+    props.alert && <div
+      className={`alert alert-${props.alert.type} alert-dismissible fade show mx-2 mt-3`}
+      role="alert"
+    >
+      <strong>{Cap(props.alert.type)}</strong>: {props.alert.msg}
+    </div>
+  );
+}
